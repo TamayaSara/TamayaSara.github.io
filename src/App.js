e8cc382d925dd6c8509cfe50815a254706c78887
Tamaya Sara <95479134+TamayaSara@users.noreply.github.com>
2023-03-11 22:29:31 -0500

    modified App.js to say {insert your name here}

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Tamaya, I'm going to master React!
         </p>
         <a
           className="App-link"
