b78e034df46aaa62d18327d790cbce34b008fc77
Tamaya Sara <95479134+TamayaSara@users.noreply.github.com>
2023-03-12 00:13:22 -0500

    added my personal image to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './TamayaSara.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={require("./TamayaSara.jpg")} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
           Tamaya, I'm going to master React!
